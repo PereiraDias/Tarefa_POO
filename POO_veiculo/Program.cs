31d3e19f3b929f87273ebd29ec8c212deaa20d6b
Pereira Dias <155259092+PereiraDias@users.noreply.github.com>
2026-05-05 16:14:56 +0100

    Add files via upload

---
+++ POO_veiculo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_veiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo carro1 = new Veiculo("LD-900-AB", "Toyota", "Lexus", 2021, 900);
+            Veiculo carro2 = new Veiculo("LD-901-CD", "Hyundai", "Tucson", 2022, 550);
+            Veiculo carro3 = new Veiculo("LD-902-EF", "Kia", "Niro", 2023, 120);
+                   
+            carro1.ActualizarQuilometragem(300);
+            carro2.ActualizarQuilometragem(150);
+            carro3.ActualizarQuilometragem(400);
+
+            carro1.ApresentarDados();
+            carro2.ApresentarDados();
+            carro3.ApresentarDados();
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ POO_veiculo/veiculo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_veiculo
+{
+    public class Veiculo
+    {
+     
+        private string matricula;
+        private string marca;
+        private string modelo;
+        private int anoFabricacao;
+        private double quilometragem;
+
+           public Veiculo()
+        {
+
+        }
+
+        public Veiculo(string matricula, string marca, string modelo, int anoFabricacao, double quilometragem)
+        {
+            this.matricula = matricula;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.anoFabricacao = anoFabricacao;
+            this.quilometragem = quilometragem;
+        }
+
+        public string Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public int AnoFabricacao
+        {
+            get { return anoFabricacao; }
+            set { anoFabricacao = value; }
+        }
+
+        public double Quilometragem
+        {
+            get { return quilometragem; }
+            set { quilometragem = value; }
+        }
+
+        public void ActualizarQuilometragem(double km)
+        {
+            quilometragem += km;
+        }
+
+        public void ApresentarDados()
+        {
+            Console.WriteLine("Matrícula: " + matricula);
+            Console.WriteLine("Marca: " + marca);
+            Console.WriteLine("Modelo: " + modelo);
+            Console.WriteLine("Ano: " + anoFabricacao);
+            Console.WriteLine("Quilometragem: " + quilometragem + " km");
+            Console.WriteLine("*******************************************");
+        }
+    }
+}
+
